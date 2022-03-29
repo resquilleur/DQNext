d5d1a14720a2295e2d8342002098ee3732f5e8da
Egor Zhukov <bit.zhukov@gmail.com>
2022-03-29 15:26:39 +0300

    Initial commit

---
+++ common.py
@@ -0,0 +1,114 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import warnings
+from datetime import timedelta, datetime
+from types import SimpleNamespace
+from typing import Iterable, Tuple, List
+
+import ptan.ptan as ptan
+import ptan.ptan.ignite as ptan_ignite
+from ignite.engine import Engine
+from ignite.metrics import RunningAverage
+from ignite.contrib.handlers import tensorboard_logger as tb_logger
+
+
+SEED = 123
+
+HYPERPARAMS = {
+    'pong': SimpleNamespace(**{
+        'env_name':         "PongNoFrameskip-v4",
+        'stop_reward':      18.0,
+        'run_name':         'pong',
+        'replay_size':      100000,
+        'replay_initial':   10000,
+        'target_net_sync':  1000,
+        'epsilon_frames':   10**5,
+        'epsilon_start':    1.0,
+        'epsilon_final':    0.02,
+        'learning_rate':    0.0001,
+        'gamma':            0.99,
+        'batch_size':       32
+    }),
+    'breakout-small': SimpleNamespace(**{
+        'env_name':         "BreakoutNoFrameskip-v4",
+        'stop_reward':      500.0,
+        'run_name':         'breakout-small',
+        'replay_size':      3*10 ** 5,
+        'replay_initial':   20000,
+        'target_net_sync':  1000,
+        'epsilon_frames':   10 ** 6,
+        'epsilon_start':    1.0,
+        'epsilon_final':    0.1,
+        'learning_rate':    0.0001,
+        'gamma':            0.99,
+        'batch_size':       64
+    }),
+    'breakout': SimpleNamespace(**{
+        'env_name':         "BreakoutNoFrameskip-v4",
+        'stop_reward':      500.0,
+        'run_name':         'breakout',
+        'replay_size':      10 ** 6,
+        'replay_initial':   50000,
+        'target_net_sync':  10000,
+        'epsilon_frames':   10 ** 6,
+        'epsilon_start':    1.0,
+        'epsilon_final':    0.1,
+        'learning_rate':    0.00025,
+        'gamma':            0.99,
+        'batch_size':       32
+    }),
+    'invaders': SimpleNamespace(**{
+        'env_name': "SpaceInvadersNoFrameskip-v4",
+        'stop_reward': 500.0,
+        'run_name': 'breakout',
+        'replay_size': 10 ** 6,
+        'replay_initial': 50000,
+        'target_net_sync': 10000,
+        'epsilon_frames': 10 ** 6,
+        'epsilon_start': 1.0,
+        'epsilon_final': 0.1,
+        'learning_rate': 0.00025,
+        'gamma': 0.99,
+        'batch_size': 32
+    }),
+}
+
+
+def unpack_batch(batch: List[ptan.experience.ExperienceFirstLast]):
+    states, actions, rewards, dones, last_states = [], [], [], [], []
+    for exp in batch:
+        state = np.array(exp.state)
+        states.append(state)
+        actions.append(exp.action)
+        rewards.append(exp.reward)
+        dones.append(exp.last_state is None)
+        # mask the value
+        if exp.last_state is None:
+            lstate = state
+        else:
+            lstate = np.array(exp.last_state)
+        last_states.append(lstate)
+    return np.array(states, copy=False), np.array(actions), + \
+        np.array(rewards, dtype=np.float32), np.array(dones, dtype=np.uint8), + \
+        np.array(last_states, copy=False)
+
+def clac_loss_dqn(batch, net, tgt_net, gamma, device=torch.device('cpu')):
+    states, actions, rewards, dones, next_states = unpack_batch(batch)
+
+    states_v = torch.tensor(states).to(device)
+    next_states_v = torch.tensor(next_states).to(device)
+    actions_v = torch.tensor(actions).to(device)
+    rewards_v = torch.tensor(rewards).to(device)
+    done_mask = torch.BoolTensor(dones).to(device)
+
+    actions_v = actions_v.unsqueeze(-1)
+    state_action_vals = net(states_v).gather(1, actions_v)
+    state_action_vals = state_action_vals.squeeze(-1)
+    with torch.no_grad():
+        next_state_vals = tgt_net(next_states_v).max(1)[0]
+        next_state_vals[done_mask] = 0.0
+
+    bellman_vals = next_state_vals.detach() * gamma + rewards_v
+    return nn.MSELoss()(state_action_vals, bellman_vals)
+
